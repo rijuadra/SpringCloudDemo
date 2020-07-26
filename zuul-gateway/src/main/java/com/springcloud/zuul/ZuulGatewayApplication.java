ab914e3c2325bf065d14bcca8de6823d1c25ff1e
Saikat Das <rijuadra@gmail.com>
2020-07-26 17:52:04 +0530

    zuul gateway added

---
+++ zuul-gateway/src/main/java/com/springcloud/zuul/ZuulGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.springcloud.zuul;
+
+import com.springcloud.zuul.filter.SimpleFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+
+}
